482ae8726a36a5b713d7d2c8ee61528c5f1cfe27
stephanedjimli <48844535+stephanedjimli@users.noreply.github.com>
2019-03-23 11:39:33 +0100

    add router

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,21 @@
       </v-btn>
     </v-toolbar>
 
+	<!--
     <v-content>
       <HelloWorld/>
     </v-content>
+	-->
+	<p>
+		<!-- use router-link component for navigation. -->
+		<!-- specify the link by passing the `to` prop. -->
+		<!-- `<router-link>` will be rendered as an `<a>` tag by default -->
+		<router-link to="/foo">Go to Foo</router-link>
+		<router-link to="/bar">Go to Bar</router-link>
+	</p>
+	<!-- route outlet -->
+	<!-- component matched by the route will render here -->
+	<router-view></router-view>
   </v-app>
 </template>
 
@@ -33,6 +45,19 @@ export default {
   data () {
     return {
       //
+    }
+  },
+  computed: {
+    username () {
+      // We will see what `params` is shortly
+      return this.$route.params.username
+    }
+  },
+  methods: {
+    goBack () {
+      window.history.length > 1
+        ? this.$router.go(-1)
+        : this.$router.push('/')
     }
   }
 }
